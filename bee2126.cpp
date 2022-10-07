f2e5fb45aebd2fb50027ce012cef84b3f870b62a
Luan Leme <luan.leme.s35@gmail.com>
2022-10-07 07:52:47 -0300

    Add files via upload

---
+++ bee2126.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+void imprimeCaso(size_t quantCasos, size_t quanSubs, size_t idxSeq)
+{
+	std::cout << "Caso #" << quantCasos << ":\n";
+
+	if (quanSubs > 0)
+		std::cout << "Qtd.Subsequencias: " << quanSubs << "\n"
+							<< "Pos: " << idxSeq << "\n";
+	else
+		std::cout << "Nao existe subsequencia\n";
+
+	std::cout << "\n";
+}
+
+int main(int argc, char const *argv[])
+{
+	size_t quantCasos = 1;
+	std::string sequencia, subSequencia;
+
+	while (std::getline(std::cin, subSequencia) && !subSequencia.empty())
+	{
+		size_t idxSeq = 0, proxIdxSeq = 0, quantSubSequencias = 0;
+
+		std::getline(std::cin, sequencia);
+
+		while ((proxIdxSeq = sequencia.find(subSequencia, idxSeq)) != std::string::npos)
+		{
+			quantSubSequencias++;
+			idxSeq = proxIdxSeq + 1;
+		}
+
+		imprimeCaso(quantCasos++, quantSubSequencias, idxSeq);
+	}
+
+	return 0;
+}
