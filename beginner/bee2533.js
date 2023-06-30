1fa105e09e9d8bfca4d654a672d8a1a73f7309bb
Luan Leme <luan.leme.s35@gmail.com>
2023-06-30 14:01:19 -0300

    Solving problem 2533

---
+++ beginner/bee2533.js
@@ -0,0 +1,20 @@
+let input = require('fs').readFileSync('stdin', 'utf8')
+let lines = input.split('\n')
+
+for (let i = 0; i < lines.length; i++) {
+	let sum = 0
+	let sumWorkloads = 0
+	let gradesAmount = parseInt(lines[i])
+
+	if (!gradesAmount)
+		return
+
+	for (let j = 0; j < gradesAmount; j++) {
+		let [grade, workLoad] = lines[++i].split(' ').map(elem => parseInt(elem))
+		sum += grade * workLoad
+		sumWorkloads += workLoad
+	}
+
+	let api = sum / sumWorkloads / 100
+	console.log(api.toFixed(4))
+}
