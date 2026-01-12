4932d798770fa96c6a7d19497366fb5341a62f21
LamenLuan <luan.leme.s35@gmail.com>
2026-01-12 13:07:20 -0300

    Solving problem 2747

---
+++ beginner/bee2747.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Beecrowd
+{
+  public static void Main()
+  {
+    Console.WriteLine(
+      "---------------------------------------\n" +
+      "|                                     |\n" +
+      "|                                     |\n" +
+      "|                                     |\n" +
+      "|                                     |\n" +
+      "|                                     |\n" +
+      "---------------------------------------"
+    );
+  }
+}
