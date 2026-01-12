08a8fd386a4f8235e067e561ba3b3e5a467903ca
LamenLuan <luan.leme.s35@gmail.com>
2026-01-12 13:29:18 -0300

    Solving problem 2749

---
+++ beginner/bee2749.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Beecrowd
+{
+  public static void Main()
+  {
+    Console.WriteLine(
+      "---------------------------------------\n" +
+      "|x = 35                               |\n" +
+      "|                                     |\n" +
+      "|               x = 35                |\n" +
+      "|                                     |\n" +
+      "|                               x = 35|\n" +
+      "---------------------------------------"
+    );
+  }
+}
