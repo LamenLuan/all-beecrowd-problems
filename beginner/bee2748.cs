234b6e424f5a86fd308979cd9b80e76d0cf9cbae
LamenLuan <luan.leme.s35@gmail.com>
2026-01-12 13:08:58 -0300

    Solving problem 2748

---
+++ beginner/bee2748.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Beecrowd
+{
+  public static void Main()
+  {
+    Console.WriteLine(
+      "---------------------------------------\n" +
+      "|        Roberto                      |\n" +
+      "|                                     |\n" +
+      "|        5786                         |\n" +
+      "|                                     |\n" +
+      "|        UNIFEI                       |\n" +
+      "---------------------------------------"
+    );
+  }
+}
