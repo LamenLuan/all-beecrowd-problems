ee25dfd88554e16e1b9cbf810aa30aa3d2f4df25
LamenLuan <luan.leme.s35@gmail.com>
2026-01-13 17:31:29 -0300

    Solving problem 2752

---
+++ beginner/bee2752.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Beecrowd
+{
+  public static void Main()
+  {
+    Console.WriteLine(
+      "<AMO FAZER EXERCICIO NO URI>\n" +
+      "<    AMO FAZER EXERCICIO NO URI>\n" +
+      "<AMO FAZER EXERCICIO >\n" +
+      "<AMO FAZER EXERCICIO NO URI>\n" +
+      "<AMO FAZER EXERCICIO NO URI    >\n" +
+      "<AMO FAZER EXERCICIO NO URI>\n" +
+      "<          AMO FAZER EXERCICIO >\n" +
+      "<AMO FAZER EXERCICIO           >"
+    );
+  }
+}
